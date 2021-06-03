dedd8db1ba0373fd1d9094aecc6f3cd6c2117dd0
Ayush Kumar Shukla <shuklaayushkumar27@gmail.com>
2021-06-03 13:30:41 +0530

    some fix

--- src/components/navabar.component.js
+++ src/components/navabar.component.js
@@ -15,21 +15,23 @@ import {
   Button,
 } from "react-bootstrap";
 import "./nav.css";
+import "aos/dist/aos.css";
 import i1 from "./code_logo.png";
 
 const Navabar = () => {
   return (
     <div class="container">
-      <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
-        <Navbar.Brand href="/" class="logo"><img class="img-responsive" src={i1} alt="logo-img"></img>CODE</Navbar.Brand>
+      <Navbar  collapseOnSelect expand="lg" bg="white" variant="light">
+        <Navbar.Brand href="/" class="logo"><img class="img-responsive" src={i1} alt="logo-img"></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto"></Nav>
+          <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about-us">Carrer</Nav.Link>
+              <Nav.Link href="/contact-us">About Us</Nav.Link></Nav>
           <Nav>
             <Form inline>
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about-us">Carrer</Nav.Link>
-              <Nav.Link href="/contact-us">About Us</Nav.Link>
+              
               <Button variant="outline-success">Contact Us</Button>
             </Form>
           </Nav>
